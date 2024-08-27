cd1931b3052eceef1161b5be90b7b3d50bc008fb
GlitchyX3 <165683653+GlitchyX3@users.noreply.github.com>
2024-08-27 20:14:12 +0530

    Create Book.js

---
+++ backend/models/Book.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const BookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  author: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  publishedDate: {
+    type: Date,
+    required: true,
+  },
+  genre: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Book = mongoose.model('Book', BookSchema);
+
+module.exports = Book;
