7ec60b37de620093726b8d01a76988e186bd1456
GlitchyX3 <165683653+GlitchyX3@users.noreply.github.com>
2024-08-28 11:30:33 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use(cors({
 
 // MongoDB Connection
 const mongoURI = 'mongodb+srv://bookwise:BXWKueya7aclXSam@bookwise.klo2g.mongodb.net/?retryWrites=true&w=majority&appName=bookwise';
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoURI)
   .then(() => console.log('MongoDB connected successfully'))
   .catch(err => {
     console.error('MongoDB connection error:', err);
