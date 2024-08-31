3769c6206f60794b7cf65173c374b587fa556de6
GlitchyX3 <165683653+GlitchyX3@users.noreply.github.com>
2024-08-31 18:45:13 +0530

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,10 +1,11 @@
-// auth.js
 const express = require('express');
-const { registerUser } = require('../controllers/authController');
+const { registerUser, loginUser } = require('../controllers/authController');
 const router = express.Router();
 
 // Register route
 router.post('/register', registerUser);
 
-// Export the router
+// Login route
+router.post('/login', loginUser);
+
 module.exports = router;
